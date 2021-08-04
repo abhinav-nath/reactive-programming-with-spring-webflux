dea55b61aecafa131af0c9311cc45cd17286c73c
Abhinav Nath <48696735+abhinav-nath@users.noreply.github.com>
2021-08-04 23:16:44 +0530

    different ways of combining two Flux

---
+++ code/learn-reactive-spring/src/test/java/com/codecafe/reactive/flux_mono_playground/FluxCombineTest.java
@@ -0,0 +1,104 @@
+package com.codecafe.reactive.flux_mono_playground;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+
+public class FluxCombineTest {
+
+    @Test
+    public void combineUsingMerge() {
+
+        Flux<String> flux1 = Flux.just("A", "B", "C");
+        Flux<String> flux2 = Flux.just("D", "E", "F");
+
+        Flux<String> mergedFlux = Flux.merge(flux1, flux2); // merge() does not maintain the order -- see next test
+
+        StepVerifier.create(mergedFlux.log())
+                .expectSubscription()
+                .expectNext("A", "B", "C", "D", "E", "F")
+                .verifyComplete();
+    }
+
+    @Test
+    public void combineUsingMerge_withDelay() {
+
+        Flux<String> flux1 = Flux.just("A", "B", "C").delayElements(Duration.ofSeconds(1));
+        Flux<String> flux2 = Flux.just("D", "E", "F").delayElements(Duration.ofSeconds(1));
+
+        Flux<String> mergedFlux = Flux.merge(flux1, flux2);
+
+        // this will fail
+        /*
+        StepVerifier.create(mergedFlux.log())
+                .expectSubscription()
+                .expectNext("A", "B", "C", "D", "E", "F")
+                .verifyComplete();
+                */
+
+        // why ?
+
+        // run this
+        StepVerifier.create(mergedFlux.log())
+                .expectSubscription()
+                .expectNextCount(6)
+                .verifyComplete();
+
+        // because of non-blocking nature of merge()
+        // merge() does not wait for flux1 to complete before subscribing to flux2
+        // so merge() subscribes to both flux1 and flux2 at the same time
+        // this is why the order gets messed up
+
+        // hence, merge() does not maintain the order
+    }
+
+    @Test
+    public void combineUsingConcat() {
+
+        Flux<String> flux1 = Flux.just("A", "B", "C");
+        Flux<String> flux2 = Flux.just("D", "E", "F");
+
+        Flux<String> mergedFlux = Flux.concat(flux1, flux2); // concat() preserves the order -- see next test
+
+        StepVerifier.create(mergedFlux.log())
+                .expectSubscription()
+                .expectNext("A", "B", "C", "D", "E", "F")
+                .verifyComplete();
+    }
+
+    @Test
+    public void combineUsingConcat_withDelay() {
+
+        Flux<String> flux1 = Flux.just("A", "B", "C").delayElements(Duration.ofSeconds(1));
+        Flux<String> flux2 = Flux.just("D", "E", "F").delayElements(Duration.ofSeconds(1));
+
+        Flux<String> mergedFlux = Flux.concat(flux1, flux2); // concat() will not subscribe to flux2 until flux1 has emitted all the elements
+
+        StepVerifier.create(mergedFlux.log())
+                .expectSubscription()
+                .expectNext("A", "B", "C", "D", "E", "F")
+                .verifyComplete();
+    }
+
+    @Test
+    public void combineUsingZip() {
+
+        Flux<String> flux1 = Flux.just("A", "B", "C");
+        Flux<String> flux2 = Flux.just("D", "E", "F");
+
+        Flux<String> mergedFlux = Flux.zip(flux1, flux2, (e1, e2) -> {
+            return e1.concat(e2); // AD, BE, CF
+        });
+
+        // e1 - first element of flux1
+        // e2 - first element of flux2
+
+        StepVerifier.create(mergedFlux.log())
+                .expectSubscription()
+                .expectNext("AD", "BE", "CF")
+                .verifyComplete();
+    }
+
+}
